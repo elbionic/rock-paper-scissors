c770f4d8280000096497b1e389dbe01333c3b034
elbionic <info@elbionic.com>
2024-09-17 01:28:16 +0200

    Add a getHumanChoice() function to the project
    Adding a functon called getHumanChoice() which has the
    task of returning "rock", "paper" or "scissors" based on the
    input of the player.
    
    Input "r" for "rock",
    "p" for "paper",
    or "s" for "scissors".

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // 1. Write a function that randomly returns "rock", "paper", "scissors".
 // 2. This is done by randomly picking an index of an array containing the 3 terms
 
-function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random()*3);
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice = "";
     if (randomNumber === 0) {
         computerChoice = "rock";
@@ -19,11 +19,32 @@ function getComputerChoice () {
 }
 
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
 // console.log(getComputerChoice());
 // console.log(getComputerChoice());
 // console.log(getComputerChoice());
 // console.log(getComputerChoice());
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let humanInput = "";
+    humanInput = prompt(`Please choose "rock" by typing "r"
+        "paper" by typing "p"
+        or "scissors" by typing "s"`, ``);
+    let humanChoice = "";
+    if  (humanInput === "r") {
+        humanChoice = "rock";
+    } else if (humanInput === "p") {
+        humanChoice = "paper";
+    } else if (humanInput === "s") {
+        humanChoice = "scissors";
+    } else {
+        console.log(`Please only use "r", "p" or "s" for a valid input!`)
+    }
+    return humanChoice;
+}
+
+
+    console.log(getHumanChoice());
