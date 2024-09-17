701887418fa7fcbc20c1cdb953d827954dfbb0e1
elbionic <info@elbionic.com>
2024-09-17 03:44:07 +0200

    Add the final two functions playRound() and playGame()
    First the function playRound() is added, which let's the user
    play one round of "rock, paper, scissors" against the computer.
    Than displays the result in the console.
    
    Than the playGame() function is added. Which calls playRound()
    five times and displays a message for the winner in the end.

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
 // 1. Write a function that randomly returns "rock", "paper", "scissors".
 // 2. This is done by randomly picking an index of an array containing the 3 terms
 
+
+// Globale variable declaration and initialisation
+let computerChoice = "";
+let humanChoice = "";
+let humanScore = 0;
+let computerScore = 0;
+
+
+// Functions declaration
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    let computerChoice = "";
+
     if (randomNumber === 0) {
         computerChoice = "rock";
     } else if (randomNumber === 1) {
@@ -19,22 +28,14 @@ function getComputerChoice() {
 }
 
 
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-// console.log(getComputerChoice());
-
 function getHumanChoice() {
     let humanInput = "";
+
     humanInput = prompt(`Please choose "rock" by typing "r"
         "paper" by typing "p"
         or "scissors" by typing "s"`, ``);
-    let humanChoice = "";
-    if  (humanInput === "r") {
+
+    if (humanInput === "r") {
         humanChoice = "rock";
     } else if (humanInput === "p") {
         humanChoice = "paper";
@@ -47,4 +48,50 @@ function getHumanChoice() {
 }
 
 
-    console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === humanChoice) {
+        console.log("Tie, try again!");
+    } else if (computerChoice === "rock" && humanChoice === "paper") {
+        humanScore = humanScore + 1;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        computerScore = computerScore + 1;
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        computerScore = computerScore + 1;
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        humanScore = humanScore + 1;
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore = humanScore + 1;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore = computerScore + 1;
+    } else {
+        console.log("An error occured!");
+    }
+    console.log(`Your choise was ${humanChoice} and the computer chose ${computerChoice}`);
+    console.log(`Your score is ${humanScore}`);
+    console.log(`The score of the computer is ${computerScore}`);
+}
+
+
+function playGame() {
+    let gameRounds = 0;
+    while (gameRounds < 5) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        gameRounds = gameRounds + 1;
+    }
+    if (humanScore < computerScore) {
+        console.log("---------------------------------");
+        console.log("Sorry but you have lost the game!");
+    } else if (humanScore > computerScore) {
+        console.log("---------------------------------");
+        console.log("Congrats, you have won!");
+    } else {
+        console.log("---------------------------------");
+        console.log("It's a human, machine tie!");
+    }
+}
+
+
+// Invoke the function
+playGame();
