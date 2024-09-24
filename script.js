1bf1a2a3b077b4e8b9e4f887fff0e90c3f6d4251
elbionic <info@elbionic.com>
2024-09-24 22:36:22 +0200

    Add a basic userinterface to the game
    Adding three buttons for your selection to the userinterface.
    Adding dynamic html which fires when you make an input.
    The dynamic html displays your choices the choice of the
    computer and the current score.
    This makes it easier to play the game.

--- script.js
+++ script.js
@@ -6,9 +6,60 @@
 
 // Globale variable declaration and initialisation
 let computerChoice = "";
-let humanChoice = "";
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice;
+
+
+
+// function getHumanChoice() {
+
+//     // AFTER working this out make a group call out of this repeating 
+//     // code like tought in the lesso on event handlers
+
+//     let rockBtn = document.querySelector("#rockBtn");
+//     rockBtn.addEventListener("click", () => {
+//         humanChoice = "rock";
+//         console.log(`You chose: ${humanChoice}`);
+//         return humanChoice;
+//     });
+
+//     let paperBtn = document.querySelector("#paperBtn");
+//     paperBtn.addEventListener("click", () => {
+//         humanChoice = "paper";
+//         console.log(`You chose: ${humanChoice}`);
+//         return humanChoice;
+//     });
+
+//     let scissorsBtn = document.querySelector("#scissorsBtn");
+//     scissorsBtn.addEventListener("click", () => {
+//         humanChoice = "scissors";
+//         console.log(`You chose: ${humanChoice}`);
+//         return humanChoice;
+//     });
+//     return humanChoice;
+// }
+
+// This is a shorter more performance and memory safing way to do
+// the same as the commented code function getHumanChoice(); above
+function getHumanChoice() {
+    const buttons = document.querySelectorAll("button");
+    // we use the .forEach method to iterate through each button
+    buttons.forEach((button) => {
+        // and for each one we add a 'click' listener
+        button.addEventListener("click", () => {
+            console.log(`You chose: ${button.id}`);
+            humanChoice = button.id;
+            computerChoice = getComputerChoice();
+            // console.log(humanChoice);
+            playRound(humanChoice, computerChoice);
+            // return humanChoice;
+        });
+        return humanChoice;
+    });
+    return humanChoice;
+}
+
 
 
 // Functions declaration
@@ -24,31 +75,19 @@ function getComputerChoice() {
     } else {
         console.log("Some unknown error has occured!")
     }
+    console.log(`The computer chose: ${computerChoice}`);
     return computerChoice;
 }
 
-
-function getHumanChoice() {
-    let humanInput = "";
-
-    humanInput = prompt(`Please choose "rock" by typing "r"
-        "paper" by typing "p"
-        or "scissors" by typing "s"`, ``);
-
-    if (humanInput === "r") {
-        humanChoice = "rock";
-    } else if (humanInput === "p") {
-        humanChoice = "paper";
-    } else if (humanInput === "s") {
-        humanChoice = "scissors";
-    } else {
-        console.log(`Please only use "r", "p" or "s" for a valid input!`)
-    }
-    return humanChoice;
-}
+// computerChoice = getComputerChoice();
+// console.log(computerChoice);
+// humanChoice = getHumanChoice();
+getHumanChoice();
 
 
 function playRound(humanChoice, computerChoice) {
+
+
     if (computerChoice === humanChoice) {
         console.log("Tie, try again!");
     } else if (computerChoice === "rock" && humanChoice === "paper") {
@@ -66,32 +105,38 @@ function playRound(humanChoice, computerChoice) {
     } else {
         console.log("An error occured!");
     }
-    console.log(`Your choise was ${humanChoice} and the computer chose ${computerChoice}`);
-    console.log(`Your score is ${humanScore}`);
-    console.log(`The score of the computer is ${computerScore}`);
+    let container = document.querySelector(".container");
+    const para = document.createElement("p");
+    para.setAttribute('style', 'white-space: pre;');
+    para.textContent = `Your choise was ${humanChoice} and the computer chose ${computerChoice} \r\n`;
+    para.textContent += `Your score is ${humanScore} \r\n`;
+    para.textContent += `The score of the computer is ${computerScore}`;
+    container.appendChild(para);
 }
 
 
-function playGame() {
-    let gameRounds = 0;
-    while (gameRounds < 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        gameRounds = gameRounds + 1;
-    }
-    if (humanScore < computerScore) {
-        console.log("---------------------------------");
-        console.log("Sorry but you have lost the game!");
-    } else if (humanScore > computerScore) {
-        console.log("---------------------------------");
-        console.log("Congrats, you have won!");
-    } else {
-        console.log("---------------------------------");
-        console.log("It's a human, machine tie!");
-    }
-}
+
+// function playGame() {
+//     let gameRounds = 0;
+//     while (gameRounds < 5) {
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//         gameRounds = gameRounds + 1;
+//     }
+//     if (humanScore < computerScore) {
+//         console.log("---------------------------------");
+//         console.log("Sorry but you have lost the game!");
+//     } else if (humanScore > computerScore) {
+//         console.log("---------------------------------");
+//         console.log("Congrats, you have won!");
+//     } else {
+//         console.log("---------------------------------");
+//         console.log("It's a human, machine tie!");
+//     }
+// }
 
 
-// Invoke the function
-playGame();
+// // Invoke the function
+// playGame();
+
