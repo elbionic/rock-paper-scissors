f46543d0be872fda22eae4a986e22b8a4cf8a63b
elbionic <info@elbionic.com>
2024-09-25 21:00:59 +0200

    Add and change code to play until score 5
    Added and refactored the code with the help of a
    youtube video, so it displays the one which has 5
    points first. Added dynamically populated HTML in
    JavaScript.

--- script.js
+++ script.js
@@ -1,142 +1,120 @@
-// console.log("Hello World");
+const userChoiceDisplay = document.createElement('h1');
+const computerChoiceDisplay = document.createElement('h1');
+const resultDisplay = document.createElement('h1');
+const playUntil5PointsDisplay = document.createElement('h1');
+playUntil5PointsDisplay.setAttribute('style', 'white-space: pre;');
+const gameGrid = document.getElementById('game');
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay, playUntil5PointsDisplay);
 
-// 1. Write a function that randomly returns "rock", "paper", "scissors".
-// 2. This is done by randomly picking an index of an array containing the 3 terms
 
-
-// Globale variable declaration and initialisation
-let computerChoice = "";
-let humanScore = 0;
+const choices = ['rock', 'paper', 'scissors'];
+let userChoice;
+let computerChoice;
 let computerScore = 0;
-let humanChoice;
+let userScore = 0;
+
+const handleClick = (event) => {
+    userChoice = event.target.id;
+    userChoiceDisplay.textContent = 'User choice: ' + userChoice;
+    generateComputerChoice();
+    getResults();
+    playUntil5Points();
+}
+
+const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    computerChoice = randomChoice;
+    computerChoiceDisplay.textContent = 'Computer choice: ' + randomChoice; 
+}
+
+for (let i = 0; i < choices.length; i++) {
+    const button = document.createElement('button');
+    button.id = choices[i];
+    button.textContent = choices[i];
+    button.addEventListener('click', handleClick);
+    gameGrid.appendChild(button);
+}
+
+const getResults = () => {
+    switch (userChoice + computerChoice) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.textContent = "YOU WIN!"  
+            userScore += 1;
+            break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.textContent = "YOU LOSE!"
+            computerScore += 1;
+            break
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+            resultDisplay.textContent = "IT\'S A DRAW!!"
+            break
+    }
+}
+
+const playUntil5Points = () => {
+        if (userScore < 5 && computerScore < 5) {
+            playUntil5PointsDisplay.textContent = `Your score is ${userScore} the computer's score is ${computerScore}\r\n`;
+            playUntil5PointsDisplay.textContent += `Go on playing! We play until 5 points are reached!`;
+        } else if (userScore === 5 && computerScore < userScore) {
+            playUntil5PointsDisplay.textContent = `Your score is ${userScore} the computer scored ${computerScore}.\r\n`; 
+            playUntil5PointsDisplay.textContent += `YOU WON! \r\n`;
+            playUntil5PointsDisplay.textContent += `Refresh the site to play again!`;
+        } else if (computerScore === 5 && userScore < computerScore) {
+            playUntil5PointsDisplay.textContent = `Your score is ${userScore} the computer scored ${computerScore}.\r\n`;
+            playUntil5PointsDisplay.textContent += `YOU LOST! \r\n`;
+            playUntil5PointsDisplay.textContent += `Refresh the site to play again!`;
+        } else {
+            playUntil5PointsDisplay.textContent = `Refresh the site to play again, HOMEY!`;
+        }
+}
 
 
 
-// function getHumanChoice() {
 
-//     // AFTER working this out make a group call out of this repeating 
-//     // code like tought in the lesso on event handlers
 
-//     let rockBtn = document.querySelector("#rockBtn");
-//     rockBtn.addEventListener("click", () => {
-//         humanChoice = "rock";
-//         console.log(`You chose: ${humanChoice}`);
-//         return humanChoice;
-//     });
 
-//     let paperBtn = document.querySelector("#paperBtn");
-//     paperBtn.addEventListener("click", () => {
-//         humanChoice = "paper";
-//         console.log(`You chose: ${humanChoice}`);
-//         return humanChoice;
-//     });
 
-//     let scissorsBtn = document.querySelector("#scissorsBtn");
-//     scissorsBtn.addEventListener("click", () => {
-//         humanChoice = "scissors";
-//         console.log(`You chose: ${humanChoice}`);
-//         return humanChoice;
-//     });
-//     return humanChoice;
+// possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
+//     userChoice = event.target.id;
+//     userChoiceDisplay.textContent = userChoice;
+//     generateComputerChoice();
+//     getResult();
+// }))
+
+// function generateComputerChoice() {
+//     const randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+//     if (randomNumber === 1) {
+//         computerChoice = 'rock';
+//     } else if (randomNumber === 2) {
+//         computerChoice = 'scissors';
+//     } else if (randomNumber === 3) {
+//         computerChoice = 'paper';
+//     }
+//     computerChoiceDisplay.textContent = computerChoice;
 // }
 
-// This is a shorter more performance and memory safing way to do
-// the same as the commented code function getHumanChoice(); above
-function getHumanChoice() {
-    const buttons = document.querySelectorAll("button");
-    // we use the .forEach method to iterate through each button
-    buttons.forEach((button) => {
-        // and for each one we add a 'click' listener
-        button.addEventListener("click", () => {
-            console.log(`You chose: ${button.id}`);
-            humanChoice = button.id;
-            computerChoice = getComputerChoice();
-            // console.log(humanChoice);
-            playRound(humanChoice, computerChoice);
-            // return humanChoice;
-        });
-        return humanChoice;
-    });
-    return humanChoice;
-}
-
-
-
-// Functions declaration
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-
-    if (randomNumber === 0) {
-        computerChoice = "rock";
-    } else if (randomNumber === 1) {
-        computerChoice = "paper";
-    } else if (randomNumber === 2) {
-        computerChoice = "scissors";
-    } else {
-        console.log("Some unknown error has occured!")
-    }
-    console.log(`The computer chose: ${computerChoice}`);
-    return computerChoice;
-}
-
-// computerChoice = getComputerChoice();
-// console.log(computerChoice);
-// humanChoice = getHumanChoice();
-getHumanChoice();
-
-
-function playRound(humanChoice, computerChoice) {
-
-
-    if (computerChoice === humanChoice) {
-        console.log("Tie, try again!");
-    } else if (computerChoice === "rock" && humanChoice === "paper") {
-        humanScore = humanScore + 1;
-    } else if (computerChoice === "rock" && humanChoice === "scissors") {
-        computerScore = computerScore + 1;
-    } else if (computerChoice === "paper" && humanChoice === "rock") {
-        computerScore = computerScore + 1;
-    } else if (computerChoice === "paper" && humanChoice === "scissors") {
-        humanScore = humanScore + 1;
-    } else if (computerChoice === "scissors" && humanChoice === "rock") {
-        humanScore = humanScore + 1;
-    } else if (computerChoice === "scissors" && humanChoice === "paper") {
-        computerScore = computerScore + 1;
-    } else {
-        console.log("An error occured!");
-    }
-    let container = document.querySelector(".container");
-    const para = document.createElement("p");
-    para.setAttribute('style', 'white-space: pre;');
-    para.textContent = `Your choise was ${humanChoice} and the computer chose ${computerChoice} \r\n`;
-    para.textContent += `Your score is ${humanScore} \r\n`;
-    para.textContent += `The score of the computer is ${computerScore}`;
-    container.appendChild(para);
-}
-
-
-
-// function playGame() {
-//     let gameRounds = 0;
-//     while (gameRounds < 5) {
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//         gameRounds = gameRounds + 1;
+// function getResult() {
+//     if (computerChoice === userChoice) {
+//         result = 'it\'s a draw!';
+//     } else if (computerChoice === 'rock' && userChoice === 'paper') {
+//         result = 'you\'ve won!';
+//     } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+//         result = 'you\'ve lost!';
+//     } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+//         result = 'you\'ve won!';
+//     } else if (computerChoice === 'paper' && userChoice === 'rock') {
+//         result = 'you\'ve lost!';
+//     } else if (computerChoice === 'scissors' && userChoice === 'paper') {
+//         result = 'you\'ve lost!';
+//     } else if (computerChoice === 'scissors' && userChoice === 'rock') {
+//         result = 'you\'ve won!';
 //     }
-//     if (humanScore < computerScore) {
-//         console.log("---------------------------------");
-//         console.log("Sorry but you have lost the game!");
-//     } else if (humanScore > computerScore) {
-//         console.log("---------------------------------");
-//         console.log("Congrats, you have won!");
-//     } else {
-//         console.log("---------------------------------");
-//         console.log("It's a human, machine tie!");
-//     }
+//     resultDisplay.textContent = result;
 // }
-
-
-// // Invoke the function
-// playGame();
-
